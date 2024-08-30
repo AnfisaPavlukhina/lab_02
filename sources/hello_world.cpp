c290389bf442bd38da74f4498c6638526cdf81bc
AnfisaPavlukhina <anfisapavluhina58576@gmail.com>
2024-08-30 10:12:18 -0400

    Clang Mozilla format

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
-
-int main(){
-    std::string res;
-    std::cin >> res;
-    std::cout << "Hello world from " << res;
-    return 1;
+int
+main()
+{
+  std::string res;
+  std::cin >> res;
+  std::cout << "Hello World from" << res;
+  return 1;
 }
